4e0828e72e4752602b6041a6f3bd8d7c79594b29
zelu <zelu@zelu-2.local>
2014-10-11 15:48:52 +0200

    Update color and accessory controllers to use bootstrap extension

--- src/Re/ConfiguratorBundle/Controller/AccessoryController.php
+++ src/Re/ConfiguratorBundle/Controller/AccessoryController.php
@@ -5,8 +5,13 @@ namespace Re\ConfiguratorBundle\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Pagerfanta\Pagerfanta;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
+use Pagerfanta\View\TwitterBootstrapView;
+
 use Re\ConfiguratorBundle\Entity\Accessory;
 use Re\ConfiguratorBundle\Form\AccessoryType;
+use Re\ConfiguratorBundle\Form\AccessoryFilterType;
 
 /**
  * Accessory controller.
@@ -14,35 +19,111 @@ use Re\ConfiguratorBundle\Form\AccessoryType;
  */
 class AccessoryController extends Controller
 {
-
     /**
      * Lists all Accessory entities.
      *
      */
     public function indexAction()
     {
-        $em = $this->getDoctrine()->getManager();
+        list($filterForm, $queryBuilder) = $this->filter();
 
-        $entities = $em->getRepository('ReConfiguratorBundle:Accessory')->findAll();
+        list($entities, $pagerHtml) = $this->paginator($queryBuilder);
 
         return $this->render('ReConfiguratorBundle:Accessory:index.html.twig', array(
             'entities' => $entities,
+            'pagerHtml' => $pagerHtml,
+            'filterForm' => $filterForm->createView(),
         ));
     }
+
+    /**
+    * Create filter form and process filter request.
+    *
+    */
+    protected function filter()
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+        $filterForm = $this->createForm(new AccessoryFilterType());
+        $em = $this->getDoctrine()->getManager();
+        $queryBuilder = $em->getRepository('ReConfiguratorBundle:Accessory')->createQueryBuilder('e');
+
+        // Reset filter
+        if ($request->get('filter_action') == 'reset') {
+            $session->remove('AccessoryControllerFilter');
+        }
+
+        // Filter action
+        if ($request->get('filter_action') == 'filter') {
+            // Bind values from the request
+            $filterForm->bind($request);
+
+            if ($filterForm->isValid()) {
+                // Build the query from the given form object
+                $this->get('lexik_form_filter.query_builder_updater')->addFilterConditions($filterForm, $queryBuilder);
+                // Save filter to session
+                $filterData = $filterForm->getData();
+                $session->set('AccessoryControllerFilter', $filterData);
+            }
+        } else {
+            // Get filter from session
+            if ($session->has('AccessoryControllerFilter')) {
+                $filterData = $session->get('AccessoryControllerFilter');
+                $filterForm = $this->createForm(new AccessoryFilterType(), $filterData);
+                $this->get('lexik_form_filter.query_builder_updater')->addFilterConditions($filterForm, $queryBuilder);
+            }
+        }
+
+        return array($filterForm, $queryBuilder);
+    }
+
+    /**
+    * Get results from paginator and get paginator view.
+    *
+    */
+    protected function paginator($queryBuilder)
+    {
+        // Paginator
+        $adapter = new DoctrineORMAdapter($queryBuilder);
+        $pagerfanta = new Pagerfanta($adapter);
+        $currentPage = $this->getRequest()->get('page', 1);
+        $pagerfanta->setCurrentPage($currentPage);
+        $entities = $pagerfanta->getCurrentPageResults();
+
+        // Paginator - route generator
+        $me = $this;
+        $routeGenerator = function($page) use ($me)
+        {
+            return $me->generateUrl('accessory', array('page' => $page));
+        };
+
+        // Paginator - view
+        $translator = $this->get('translator');
+        $view = new TwitterBootstrapView();
+        $pagerHtml = $view->render($pagerfanta, $routeGenerator, array(
+            'proximity' => 3,
+            'prev_message' => $translator->trans('views.index.pagprev', array(), 'JordiLlonchCrudGeneratorBundle'),
+            'next_message' => $translator->trans('views.index.pagnext', array(), 'JordiLlonchCrudGeneratorBundle'),
+        ));
+
+        return array($entities, $pagerHtml);
+    }
+
     /**
      * Creates a new Accessory entity.
      *
      */
     public function createAction(Request $request)
     {
-        $entity = new Accessory();
-        $form = $this->createCreateForm($entity);
-        $form->handleRequest($request);
+        $entity  = new Accessory();
+        $form = $this->createForm(new AccessoryType(), $entity);
+        $form->bind($request);
 
         if ($form->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $em->persist($entity);
             $em->flush();
+            $this->get('session')->getFlashBag()->add('success', 'flash.create.success');
 
             return $this->redirect($this->generateUrl('accessory_show', array('id' => $entity->getId())));
         }
@@ -54,32 +135,13 @@ class AccessoryController extends Controller
     }
 
     /**
-     * Creates a form to create a Accessory entity.
-     *
-     * @param Accessory $entity The entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createCreateForm(Accessory $entity)
-    {
-        $form = $this->createForm(new AccessoryType(), $entity, array(
-            'action' => $this->generateUrl('accessory_create'),
-            'method' => 'POST',
-        ));
-
-        $form->add('submit', 'submit', array('label' => 'Create'));
-
-        return $form;
-    }
-
-    /**
      * Displays a form to create a new Accessory entity.
      *
      */
     public function newAction()
     {
         $entity = new Accessory();
-        $form   = $this->createCreateForm($entity);
+        $form   = $this->createForm(new AccessoryType(), $entity);
 
         return $this->render('ReConfiguratorBundle:Accessory:new.html.twig', array(
             'entity' => $entity,
@@ -105,8 +167,7 @@ class AccessoryController extends Controller
 
         return $this->render('ReConfiguratorBundle:Accessory:show.html.twig', array(
             'entity'      => $entity,
-            'delete_form' => $deleteForm->createView(),
-        ));
+            'delete_form' => $deleteForm->createView(),        ));
     }
 
     /**
@@ -123,7 +184,7 @@ class AccessoryController extends Controller
             throw $this->createNotFoundException('Unable to find Accessory entity.');
         }
 
-        $editForm = $this->createEditForm($entity);
+        $editForm = $this->createForm(new AccessoryType(), $entity);
         $deleteForm = $this->createDeleteForm($id);
 
         return $this->render('ReConfiguratorBundle:Accessory:edit.html.twig', array(
@@ -133,24 +194,6 @@ class AccessoryController extends Controller
         ));
     }
 
-    /**
-    * Creates a form to edit a Accessory entity.
-    *
-    * @param Accessory $entity The entity
-    *
-    * @return \Symfony\Component\Form\Form The form
-    */
-    private function createEditForm(Accessory $entity)
-    {
-        $form = $this->createForm(new AccessoryType(), $entity, array(
-            'action' => $this->generateUrl('accessory_update', array('id' => $entity->getId())),
-            'method' => 'PUT',
-        ));
-
-        $form->add('submit', 'submit', array('label' => 'Update'));
-
-        return $form;
-    }
     /**
      * Edits an existing Accessory entity.
      *
@@ -166,13 +209,17 @@ class AccessoryController extends Controller
         }
 
         $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createEditForm($entity);
-        $editForm->handleRequest($request);
+        $editForm = $this->createForm(new AccessoryType(), $entity);
+        $editForm->bind($request);
 
         if ($editForm->isValid()) {
+            $em->persist($entity);
             $em->flush();
+            $this->get('session')->getFlashBag()->add('success', 'flash.update.success');
 
             return $this->redirect($this->generateUrl('accessory_edit', array('id' => $id)));
+        } else {
+            $this->get('session')->getFlashBag()->add('error', 'flash.update.error');
         }
 
         return $this->render('ReConfiguratorBundle:Accessory:edit.html.twig', array(
@@ -181,6 +228,7 @@ class AccessoryController extends Controller
             'delete_form' => $deleteForm->createView(),
         ));
     }
+
     /**
      * Deletes a Accessory entity.
      *
@@ -188,7 +236,7 @@ class AccessoryController extends Controller
     public function deleteAction(Request $request, $id)
     {
         $form = $this->createDeleteForm($id);
-        $form->handleRequest($request);
+        $form->bind($request);
 
         if ($form->isValid()) {
             $em = $this->getDoctrine()->getManager();
@@ -200,6 +248,9 @@ class AccessoryController extends Controller
 
             $em->remove($entity);
             $em->flush();
+            $this->get('session')->getFlashBag()->add('success', 'flash.delete.success');
+        } else {
+            $this->get('session')->getFlashBag()->add('error', 'flash.delete.error');
         }
 
         return $this->redirect($this->generateUrl('accessory'));
@@ -210,14 +261,12 @@ class AccessoryController extends Controller
      *
      * @param mixed $id The entity id
      *
-     * @return \Symfony\Component\Form\Form The form
+     * @return Symfony\Component\Form\Form The form
      */
     private function createDeleteForm($id)
     {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('accessory_delete', array('id' => $id)))
-            ->setMethod('DELETE')
-            ->add('submit', 'submit', array('label' => 'Delete'))
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
             ->getForm()
         ;
     }
--- src/Re/ConfiguratorBundle/Controller/ColorController.php
+++ src/Re/ConfiguratorBundle/Controller/ColorController.php
@@ -5,8 +5,13 @@ namespace Re\ConfiguratorBundle\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Pagerfanta\Pagerfanta;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
+use Pagerfanta\View\TwitterBootstrapView;
+
 use Re\ConfiguratorBundle\Entity\Color;
 use Re\ConfiguratorBundle\Form\ColorType;
+use Re\ConfiguratorBundle\Form\ColorFilterType;
 
 /**
  * Color controller.
@@ -14,35 +19,111 @@ use Re\ConfiguratorBundle\Form\ColorType;
  */
 class ColorController extends Controller
 {
-
     /**
      * Lists all Color entities.
      *
      */
     public function indexAction()
     {
-        $em = $this->getDoctrine()->getManager();
+        list($filterForm, $queryBuilder) = $this->filter();
 
-        $entities = $em->getRepository('ReConfiguratorBundle:Color')->findAll();
+        list($entities, $pagerHtml) = $this->paginator($queryBuilder);
 
         return $this->render('ReConfiguratorBundle:Color:index.html.twig', array(
             'entities' => $entities,
+            'pagerHtml' => $pagerHtml,
+            'filterForm' => $filterForm->createView(),
         ));
     }
+
+    /**
+    * Create filter form and process filter request.
+    *
+    */
+    protected function filter()
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+        $filterForm = $this->createForm(new ColorFilterType());
+        $em = $this->getDoctrine()->getManager();
+        $queryBuilder = $em->getRepository('ReConfiguratorBundle:Color')->createQueryBuilder('e');
+
+        // Reset filter
+        if ($request->get('filter_action') == 'reset') {
+            $session->remove('ColorControllerFilter');
+        }
+
+        // Filter action
+        if ($request->get('filter_action') == 'filter') {
+            // Bind values from the request
+            $filterForm->bind($request);
+
+            if ($filterForm->isValid()) {
+                // Build the query from the given form object
+                $this->get('lexik_form_filter.query_builder_updater')->addFilterConditions($filterForm, $queryBuilder);
+                // Save filter to session
+                $filterData = $filterForm->getData();
+                $session->set('ColorControllerFilter', $filterData);
+            }
+        } else {
+            // Get filter from session
+            if ($session->has('ColorControllerFilter')) {
+                $filterData = $session->get('ColorControllerFilter');
+                $filterForm = $this->createForm(new ColorFilterType(), $filterData);
+                $this->get('lexik_form_filter.query_builder_updater')->addFilterConditions($filterForm, $queryBuilder);
+            }
+        }
+
+        return array($filterForm, $queryBuilder);
+    }
+
+    /**
+    * Get results from paginator and get paginator view.
+    *
+    */
+    protected function paginator($queryBuilder)
+    {
+        // Paginator
+        $adapter = new DoctrineORMAdapter($queryBuilder);
+        $pagerfanta = new Pagerfanta($adapter);
+        $currentPage = $this->getRequest()->get('page', 1);
+        $pagerfanta->setCurrentPage($currentPage);
+        $entities = $pagerfanta->getCurrentPageResults();
+
+        // Paginator - route generator
+        $me = $this;
+        $routeGenerator = function($page) use ($me)
+        {
+            return $me->generateUrl('color', array('page' => $page));
+        };
+
+        // Paginator - view
+        $translator = $this->get('translator');
+        $view = new TwitterBootstrapView();
+        $pagerHtml = $view->render($pagerfanta, $routeGenerator, array(
+            'proximity' => 3,
+            'prev_message' => $translator->trans('views.index.pagprev', array(), 'JordiLlonchCrudGeneratorBundle'),
+            'next_message' => $translator->trans('views.index.pagnext', array(), 'JordiLlonchCrudGeneratorBundle'),
+        ));
+
+        return array($entities, $pagerHtml);
+    }
+
     /**
      * Creates a new Color entity.
      *
      */
     public function createAction(Request $request)
     {
-        $entity = new Color();
-        $form = $this->createCreateForm($entity);
-        $form->handleRequest($request);
+        $entity  = new Color();
+        $form = $this->createForm(new ColorType(), $entity);
+        $form->bind($request);
 
         if ($form->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $em->persist($entity);
             $em->flush();
+            $this->get('session')->getFlashBag()->add('success', 'flash.create.success');
 
             return $this->redirect($this->generateUrl('color_show', array('id' => $entity->getId())));
         }
@@ -54,32 +135,13 @@ class ColorController extends Controller
     }
 
     /**
-     * Creates a form to create a Color entity.
-     *
-     * @param Color $entity The entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createCreateForm(Color $entity)
-    {
-        $form = $this->createForm(new ColorType(), $entity, array(
-            'action' => $this->generateUrl('color_create'),
-            'method' => 'POST',
-        ));
-
-        $form->add('submit', 'submit', array('label' => 'Create'));
-
-        return $form;
-    }
-
-    /**
      * Displays a form to create a new Color entity.
      *
      */
     public function newAction()
     {
         $entity = new Color();
-        $form   = $this->createCreateForm($entity);
+        $form   = $this->createForm(new ColorType(), $entity);
 
         return $this->render('ReConfiguratorBundle:Color:new.html.twig', array(
             'entity' => $entity,
@@ -105,8 +167,7 @@ class ColorController extends Controller
 
         return $this->render('ReConfiguratorBundle:Color:show.html.twig', array(
             'entity'      => $entity,
-            'delete_form' => $deleteForm->createView(),
-        ));
+            'delete_form' => $deleteForm->createView(),        ));
     }
 
     /**
@@ -123,7 +184,7 @@ class ColorController extends Controller
             throw $this->createNotFoundException('Unable to find Color entity.');
         }
 
-        $editForm = $this->createEditForm($entity);
+        $editForm = $this->createForm(new ColorType(), $entity);
         $deleteForm = $this->createDeleteForm($id);
 
         return $this->render('ReConfiguratorBundle:Color:edit.html.twig', array(
@@ -133,24 +194,6 @@ class ColorController extends Controller
         ));
     }
 
-    /**
-    * Creates a form to edit a Color entity.
-    *
-    * @param Color $entity The entity
-    *
-    * @return \Symfony\Component\Form\Form The form
-    */
-    private function createEditForm(Color $entity)
-    {
-        $form = $this->createForm(new ColorType(), $entity, array(
-            'action' => $this->generateUrl('color_update', array('id' => $entity->getId())),
-            'method' => 'PUT',
-        ));
-
-        $form->add('submit', 'submit', array('label' => 'Update'));
-
-        return $form;
-    }
     /**
      * Edits an existing Color entity.
      *
@@ -166,13 +209,17 @@ class ColorController extends Controller
         }
 
         $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createEditForm($entity);
-        $editForm->handleRequest($request);
+        $editForm = $this->createForm(new ColorType(), $entity);
+        $editForm->bind($request);
 
         if ($editForm->isValid()) {
+            $em->persist($entity);
             $em->flush();
+            $this->get('session')->getFlashBag()->add('success', 'flash.update.success');
 
             return $this->redirect($this->generateUrl('color_edit', array('id' => $id)));
+        } else {
+            $this->get('session')->getFlashBag()->add('error', 'flash.update.error');
         }
 
         return $this->render('ReConfiguratorBundle:Color:edit.html.twig', array(
@@ -181,6 +228,7 @@ class ColorController extends Controller
             'delete_form' => $deleteForm->createView(),
         ));
     }
+
     /**
      * Deletes a Color entity.
      *
@@ -188,7 +236,7 @@ class ColorController extends Controller
     public function deleteAction(Request $request, $id)
     {
         $form = $this->createDeleteForm($id);
-        $form->handleRequest($request);
+        $form->bind($request);
 
         if ($form->isValid()) {
             $em = $this->getDoctrine()->getManager();
@@ -200,6 +248,9 @@ class ColorController extends Controller
 
             $em->remove($entity);
             $em->flush();
+            $this->get('session')->getFlashBag()->add('success', 'flash.delete.success');
+        } else {
+            $this->get('session')->getFlashBag()->add('error', 'flash.delete.error');
         }
 
         return $this->redirect($this->generateUrl('color'));
@@ -210,14 +261,12 @@ class ColorController extends Controller
      *
      * @param mixed $id The entity id
      *
-     * @return \Symfony\Component\Form\Form The form
+     * @return Symfony\Component\Form\Form The form
      */
     private function createDeleteForm($id)
     {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('color_delete', array('id' => $id)))
-            ->setMethod('DELETE')
-            ->add('submit', 'submit', array('label' => 'Delete'))
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
             ->getForm()
         ;
     }
