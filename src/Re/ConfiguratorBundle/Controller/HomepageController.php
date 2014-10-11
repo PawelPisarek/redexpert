3845737c6cfdf11ab717b526a973370feb647754
zelu <zelu@zelu-2.local>
2014-10-11 11:08:11 +0200

    Create HomepageController

---
+++ src/Re/ConfiguratorBundle/Controller/HomepageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Re\ConfiguratorBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomepageController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('ReConfiguratorBundle:Homepage:index.html.twig', array(
+                // ...
+            ));    }
+
+}
